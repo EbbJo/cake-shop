50311de3e8b50e10ec794c930d28d9cb4a11db13
ziMMi99 <simon@mvbmail.dk>
2025-03-31 09:21:17 +0200

    Feeter

--- cake-shop-frontend/src/Components/Footer.jsx
+++ cake-shop-frontend/src/Components/Footer.jsx
@@ -3,19 +3,9 @@ import { Link } from "react-router";
 function Footer() {
 
     return(
-        <nav className="h-10">
-            <div className="flex justify-between p-5 bg-rose-800 text-pink-100 font-navbar font-bold">
-                <div>
-                    <Link to="/">Cake Shop</Link>
-                </div>
-                <div className="flex justify-between">
-                    <div className="mr-5">
-                        <Link to="/">Home</Link>
-                    </div>
-                    <div className="mr-5">
-                        <Link to="/about">About us</Link>
-                    </div>
-                </div>
+        <nav className="h-25">
+            <div className="flex p-10 mt-[65px] bg-rose-800 text-pink-50 font-navbar font-bold justify-center">
+                <p className="opacity-90">@ All Rights Reserves THE CAKE BOIS</p>
             </div>
         </nav>
     )
