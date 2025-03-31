6171f96c93322b80dbf86505fc6ceba0ef71a8fc
SebErSej <Crazi.Seb@gmail.com>
2025-03-31 10:22:43 +0200

    Added a "back" button/link to product page
    Though it doesn't remember which page you where on, on the home page

--- cake-shop-frontend/src/Pages/ProductPage.jsx
+++ cake-shop-frontend/src/Pages/ProductPage.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getCakeByID } from "../services/api.js";
+import { Link } from "react-router";
 
 function ProductPage() {
 
@@ -30,22 +31,30 @@ function ProductPage() {
     }
 
     return (
-        <div class="md:flex md:flex-row mt-10">
-            <div class="flex-1">
-                <img src={`/Images/Cake-${id}.jpg`} className="size-[500px] mx-auto rounded-lg object-cover border-2 border-rose-200 border-solid shadow-md mb-10"></img>
+        <div>
+            <div class="text-rose-700 italic my-2 ml-6">
+                
+                <Link to="/">&lt; back</Link>
+                
             </div>
-            <div className="flex-1 ml-2">
-                <div class="text-rose-800 text-3xl my-3">
-                    {product.name}
+            <div class="md:flex md:flex-row mt-8">
+                <div class="flex-1">
+                    <img src={`/Images/Cake-${id}.jpg`} className="size-[500px] mx-auto rounded-lg object-cover border-2 border-rose-200 border-solid shadow-md mb-10"></img>
                 </div>
-                <div class="text-xl my-2">
-                    ${product.price}
+                <div className="flex-1 ml-2">
+                    <div class="text-rose-800 text-3xl my-3">
+                        {product.name}
+                    </div>
+                    <div class="text-xl my-2">
+                        ${product.price}
+                    </div>
+                    <div class="text-lg">
+                        {product.description}
+                    </div>
                 </div>
-                <div class="text-lg">
-                    {product.description}  
-                </div>    
             </div>
         </div>
+
 
     )
 }
